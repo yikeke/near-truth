8ad7d63f87b13f33ab97c2b36aab58d4f46449e2
yikeke <yikeke@pingcap.com>
2021-05-26 11:55:55 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -14,6 +14,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_material
+
 # -- Project information -----------------------------------------------------
 
 project = 'near-truth'
